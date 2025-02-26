55a04f2e76454a93034ae2d2203ec506a9204b3d
suthikawong <suthika.wsrd@gmail.com>
2025-02-26 21:45:46 +0000

    create exception filter

---
+++ src/exception.filter.ts
@@ -0,0 +1,42 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class AppExceptionFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+
+    let httpStatus: number, error: string, message: string;
+    if (exception instanceof HttpException) {
+      httpStatus = exception.getStatus();
+      const res = exception.getResponse() as {
+        statusCode: number;
+        message: string;
+      };
+      error = res.message;
+      message = res.message;
+    } else {
+      httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+      error = 'Internal Server Error';
+      const res = exception as { message: string };
+      message = res.message;
+    }
+
+    const responseBody = {
+      status: httpStatus,
+      error: error,
+      message: message,
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
