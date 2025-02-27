b79970b96ed7e87fe16ec6c69840a0ec510351a7
suthikawong <suthika.wsrd@gmail.com>
2025-02-27 12:05:45 +0000

    manage return data

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,36 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+export interface Cat {
+  name: string;
+  age: number;
+  breed: string;
+}
+
+interface CreateCatDto {
+  name: string;
+  age: number;
+  breed: string;
+}
+
+export interface AppResponse<T> {
+  data: T;
+  total?: number;
+}
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
-  create(): string {
-    return 'This action adds a new cat';
+  create(@Body() createCatDto: CreateCatDto): AppResponse<Cat> {
+    const data = this.catsService.create(createCatDto);
+    return { data };
   }
 
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  findAll(): AppResponse<Cat[]> {
+    const data = this.catsService.findAll();
+    return { data, total: 1 };
   }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@nestjs/common';
-
-interface Cat {
-  name: string;
-  age: number;
-  breed: string;
-}
+import { Cat } from './cats.controller';
 
 @Injectable()
 export class CatsService {
   private readonly cats: Cat[] = [];
 
-  create(cat: Cat) {
+  create(cat: Cat): Cat {
     this.cats.push(cat);
+    return cat;
   }
 
   findAll(): Cat[] {
