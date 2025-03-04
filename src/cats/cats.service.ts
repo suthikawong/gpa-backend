522f1db54bef7733fe82e79d6e9546d4cf869e1a
suthikawong <suthika.wsrd@gmail.com>
2025-03-04 21:27:49 +0000

    change table name

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { db } from 'src/db';
-import { catTable } from 'src/db/schema/cat';
+import { catsTable } from 'src/db/schema/cat';
 import { Cat } from './cats.controller';
 
 @Injectable()
@@ -9,16 +9,16 @@ export class CatsService {
 
   async create(cat: Cat): Promise<Cat> {
     this.cats.push(cat);
-    const data: typeof catTable.$inferInsert = {
+    const data: typeof catsTable.$inferInsert = {
       name: cat.name,
       age: cat.age,
       breed: cat.breed,
     };
-    await db.insert(catTable).values(data);
+    await db.insert(catsTable).values(data);
     return cat;
   }
 
   async findAll(): Promise<Cat[]> {
-    return await db.select().from(catTable);
+    return await db.select().from(catsTable);
   }
 }
--- src/db/schema/cat.ts
+++ src/db/schema/cat.ts
@@ -1,6 +1,6 @@
 import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 
-export const catTable = pgTable('cat', {
+export const catsTable = pgTable('cats', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
