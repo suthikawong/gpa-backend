b089fb8b68e9f0e278c29f9f180dabf2ce59f3e1
suthikawong <suthika.wsrd@gmail.com>
2025-05-10 23:40:58 +0100

    set base url

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
