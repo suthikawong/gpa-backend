f4a5ae98bc9e4b434aadfdcab20743429f5f716d
suthikawong <suthika.wsrd@gmail.com>
2025-05-13 01:10:10 +0100

    add jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-// import { JwtModule } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-// import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [PassportModule, UserModule],
+  imports: [PassportModule, JwtModule, UserModule],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
